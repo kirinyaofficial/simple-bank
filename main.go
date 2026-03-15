2b6f4169e0b055d48f86d78282b4f80afbad201e
Kirinya J <kirinya21@yahoo.com>
2026-03-15 16:27:46 +0200

    connection pool setup and working on api for account

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kirinyaofficial/simple-bank/api"
+	db "github.com/kirinyaofficial/simple-bank/db/sqlc"
+)
+
+const (
+	dbSource      = "postgresql://root:secrete@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
 func main() {
 
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("Can not create connection pool", err)
+	}
+
+	if err := conn.Ping(context.Background()); err != nil {
+		log.Fatal("cannot ping database:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewSerevr(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
