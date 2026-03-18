4f229803b90506feb92a46536ab9052abef3f0cc
Kirinya J <kirinya21@yahoo.com>
2026-03-18 10:16:54 +0200

    create user api added

--- api/server.go
+++ api/server.go
@@ -21,6 +21,8 @@ func NewServer(store db.Store) *Server {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	router.POST("/users", server.createUser)
+
 	router.POST("/accounts", server.createAccount)
 	router.POST("/transfers", server.createTransfer)
 	router.GET("/accounts/:id", server.getAccount)
